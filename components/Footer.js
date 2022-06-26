cb036ebde185355ebc253bc7d345100bff0578bf
MJDJ <mjd.thif@icloud.com>
2022-06-27 00:29:43 +0200

    firsr commit Asplund

--- components/Footer.js
+++ components/Footer.js
@@ -6,9 +6,8 @@ import footerStyle from '../styles/footer.module.scss';
 const Footer = () => {
   return (
     <footer className={footerStyle.footer}>
-      
           <div> 
-            Sara portfilio ©  { new Date().getUTCFullYear() } All right reserved.
+           Asplundsväskor ©  { new Date().getUTCFullYear() } All right reserved.
           </div>
           <span>Levantisk.©.DEV </span>
     </footer>
--- components/homePages/homePage2.js
+++ components/homePages/homePage2.js
@@ -10,39 +10,45 @@ export default function HomePage2() {
 
 
       <div className={style.profileContainer}>
-               <div className={style.title}>
+          
+                  <div className={style.title}>
                       <h1>Services</h1>
-                </div>
-      <Services {...asplundServices}/>
-      <Services {...asplundServices}/>
-      <div className={style.description}>
-              <h1>
-                        Om din väska skadats under resan är du välkommen att 
-                        leverera den till vår kunniga personal. Vi samarbetar med många flygbolag såsom Finnair, 
-                        Air France, KLM och British Airways. Vi samarbetar också med workshops för att hjälpa våra kunder med andra typer av underhållsservice. 
-                        Vår serviceverkstad erbjuder olika typer av tjänster, inte bara för våra varumärken, utan också för många andra kända märken som Rimowa, 
-                        Roncato Tumi och många fler. Välkommen in
-              </h1>
+                            <h2>
+                              Om din väska skadats under resan är du välkommen att 
+                              leverera den till vår kunniga personal. Vi samarbetar med många flygbolag såsom Finnair, 
+                              Air France, KLM och British Airways. Vi samarbetar också med workshops för att hjälpa våra kunder med andra typer av underhållsservice. 
+                              Vår serviceverkstad erbjuder olika typer av tjänster, inte bara för våra varumärken, utan också för många andra kända märken som Rimowa, 
+                              Roncato Tumi och många fler. Välkommen in
+                        </h2>
+                    </div>
+        
+          <div>
+              <Services {...asplundServices}/>
+                <Services {...asplundServices}/>
+          </div>    
       </div>
-                
-      </div>
-        <div className={style.profileContainer} >
+
+      <div className={style.profileContainer}>
+ 
                 <div className={style.title}>
-                      <h1>Produkter</h1>
-                </div>
-          <Services {...asplundServices}/>
-          <Services {...asplundServices}/>
-      
-            <div className={style.description}>
-                  <h1>
-                          Om din väska skadats under resan är du välkommen att 
-                          leverera den till vår kunniga personal. Vi samarbetar med många flygbolag såsom Finnair, 
-                          Air France, KLM och British Airways. Vi samarbetar också med workshops för att hjälpa våra kunder med andra typer av underhållsservice. 
-                          Vår serviceverkstad erbjuder olika typer av tjänster, inte bara för våra varumärken, utan också för många andra kända märken som Rimowa, 
-                          Roncato Tumi och många fler. Välkommen in
-                  </h1>
-            </div>
-        </div>
+                      <h1>Services</h1>
+                            <h2>
+                              Om din väska skadats under resan är du välkommen att 
+                              leverera den till vår kunniga personal. Vi samarbetar med många flygbolag såsom Finnair, 
+                              Air France, KLM och British Airways. Vi samarbetar också med workshops för att hjälpa våra kunder med andra typer av underhållsservice. 
+                              Vår serviceverkstad erbjuder olika typer av tjänster, inte bara för våra varumärken, utan också för många andra kända märken som Rimowa, 
+                              Roncato Tumi och många fler. Välkommen in
+                        </h2>
+                    </div>
+          <div>
+                <Services {...asplundServices}/>
+                <Services {...asplundServices}/>
+          </div> 
+        
+          <div>
+           
+          </div>    
+      </div>
     </div>
 
  
